404c04f898a19344986e3c19716149471db3029d
gauthamksn <gsnair@adobe.com>
2025-11-20 21:36:21 +0530

    feat: add firebase authentication

---
+++ src/app/ui/components/signIn.js
@@ -0,0 +1,64 @@
+"use client";
+
+import { deleteCookie, setCookie } from "cookies-next";
+
+import { useEffect } from "react";
+
+import {
+  onIdTokenChanged,
+  signInWithGoogle,
+  signOut,
+} from "@/app/lib/firebase/auth";
+
+function useUserSession(initialUser) {
+  useEffect(() => {
+    return onIdTokenChanged(async (user) => {
+      if (user) {
+        const idToken = await user.getIdToken();
+        await setCookie("__session", idToken);
+      } else {
+        await deleteCookie("__session");
+      }
+      if (initialUser?.uid === user?.uid) {
+        return;
+      }
+      window.location.reload();
+    });
+  }, [initialUser]);
+
+  return initialUser;
+}
+
+export default function SignIn({ initialUser }) {
+  const user = useUserSession(initialUser);
+
+  const handleSignOut = (event) => {
+    event.preventDefault();
+    signOut();
+  };
+
+  const handleSignIn = (event) => {
+    event.preventDefault();
+    signInWithGoogle();
+  };
+
+  return (
+    <div>
+      {user ? (
+        <button
+          className="bg-primary rounded-md px-4 py-2 text-white"
+          onClick={handleSignOut}
+        >
+          Sign out
+        </button>
+      ) : (
+        <button
+          className="bg-primary rounded-md px-4 py-2 text-white"
+          onClick={handleSignIn}
+        >
+          Sign in with Google
+        </button>
+      )}
+    </div>
+  );
+}
