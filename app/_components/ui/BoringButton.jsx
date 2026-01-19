39835d43f8ee64c60c10a86e904ebe8cd7e66202
Sandra <sandrapp0241@gmail.com>
2026-01-19 20:24:14 +0530

    feat: nickname and connect flow

--- app/_components/ui/BoringButton.jsx
+++ app/_components/ui/BoringButton.jsx
@@ -1,14 +1,13 @@
 export default function BoringButton({ children, action, className }) {
   return (
-    <form action={action}>
-      <button
-        type="submit"
-        className={
-          className || "px-4 py-2 bg-primary text-white font-bold text-base"
-        }
-      >
-        {children}
-      </button>
-    </form>
+    <button
+      onClick={action}
+      type="submit"
+      className={
+        className || "px-4 py-2 bg-primary text-white font-bold text-base"
+      }
+    >
+      {children}
+    </button>
   );
 }
